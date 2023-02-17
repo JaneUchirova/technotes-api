232f16b2bc10f7e685c3c44de75b917ca4253981
JaneUchirova <67915258+JaneUchirova@users.noreply.github.com>
2023-02-17 19:50:26 +0200

    Update noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const notesController = require('../controllers/notesController')
-const verifyJWT = require('../middleware/verifyJWT')
+const verifyJWT = require('../middleware/veryfyJWT')
 
-router.use(verifyJWT)
+router.use(veryfyJWT)
 
 router.route('/')
     .get(notesController.getAllNotes)
